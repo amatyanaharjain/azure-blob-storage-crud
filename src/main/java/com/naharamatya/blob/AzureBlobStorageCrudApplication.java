41bc872883adf590048111904e4d39f0cbf92236
amatyanaharjain <anahar@volvocars.com>
2024-07-27 10:07:29 +0000

    initial commit

---
+++ src/main/java/com/naharamatya/blob/AzureBlobStorageCrudApplication.java
@@ -0,0 +1,13 @@
+package com.naharamatya.blob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureBlobStorageCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureBlobStorageCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naharamatya/blob/AzureBlobStorageCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naharamatya.blob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureBlobStorageCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
